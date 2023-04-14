2a20ef0d0eff8a612702555559ba8dbc5503141d
Adrien Tremblay <adrien@adrientremblay.com>
2023-04-14 14:42:13 -0400

    Can close window

--- src/main.cpp
+++ src/main.cpp
@@ -4,8 +4,13 @@
 int main() {
     sf::Window window(sf::VideoMode(800, 600), "My window");
 
-    std::string balls;
-    std::cin >> balls;
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+    }
 
     return 0;
 }
