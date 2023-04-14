d18035fb25cc8c3ecf2ee8f9ad145393d9363922
Adrien Tremblay <adrien@adrientremblay.com>
2023-04-14 14:39:25 -0400

    Draw a window

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,11 @@
+#include <SFML/Window.hpp>
+#include <iostream>
+
 int main() {
+    sf::Window window(sf::VideoMode(800, 600), "My window");
+
+    std::string balls;
+    std::cin >> balls;
+
     return 0;
 }
